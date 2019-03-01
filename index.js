7036c544ddd2373c3d8e18d9cefc6b681d600f0f
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-01 15:35:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const rp = require('request-promise-native');
+const services_url = 'https://api.devhub.virginia.edu/v1/library/services/';
 module.exports = {
     test: function(){
         console.log("Haiylu is testing!");
