f0055ae71800ea605965661fa731406629dd5568
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-05 11:16:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 const rp = require('request-promise-native ');
 function servicesTest(agent){
     var final=" ";
-    var varOptions={ url:'https://api.devhub.virginia.edu/v1/library/services/', 
+    var hk={ url:'https://api.devhub.virginia.edu/v1/library/services/', 
                  headers:
               { 'User-Agent': 'Request-Promise'},JSON: true};
            
+return rp(hk)
+    		.then(function (services) {
+			console.log(services);
+        		final = services.length;
+			agent.add(final);
+    		})
+		.catch(function (err) {
+        		final = err;
+			agent.add(final);
+    		});
+	return Promise.resolve(agent);
+}
+
+
+
+module.exports = {
+		servicesTest:servicesTest
 }
 */
