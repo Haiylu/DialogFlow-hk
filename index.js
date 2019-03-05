bc23d5ea958f602fae658d4138c1200aa01615da
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-05 11:11:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-
-/*const rp = require('request-promise-native');
-const services_url = 'https://api.devhub.virginia.edu/v1/library/services/';
-*/
 /*
 const rp = require('request-promise-native ');
-//const services_url='https://api.devhub.virginia.edu/v1/library/services/';
 function servicesTest(agent){
     var final=" ";
     var varOptions={ url:'https://api.devhub.virginia.edu/v1/library/services/', 
