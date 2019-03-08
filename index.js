126c2cb8a4924987814e86c3d39a368edea2269f
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-07 22:30:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+/*
+const functions=require('firebase-functions')
+const {dialogflow}=require('actions-on-google')
+
+const WELLCOME_INTENT='Default Welcome Intent'
+const FALLBACK_INTENT='Default Fallback Intent'
+const LIBRARY_SERVICES_INTENT='Library_Services'
+
+const app=dialogflow()
+app.intent(WELLCOME_INTENT, (conv)={
+	conv.ask("Wellcome to UVA Library Services! Ask for archives info") 		
+})
+app.intent(FALLBACK_INTENT, (conv)=> {
+	conv.ask("I didn't understand your request")	
+})
+app.intent(LIBRARY_SERVICES_INTENT, (conv) =>{
+     conv.ask("http://small.library.virginia.edu/collections/university-of-virginia-archives/")
+})
+exports.dialogflowFirebaseFulfillment=functions.https.onRequest(app)
+
+*/
 
 /*const rp = require('request-promise-native ');
 function servicesTest(agent){
@@ -28,24 +49,4 @@ module.exports = {
 //Library_Services
 */
 
-/*
-const functions=require('firebase-functions')
-const {dialogflow}=require('actions-on-google')
 
-const WELLCOME_INTENT='Default Welcome Intent'
-const FALLBACK_INTENT='Default Fallback Intent'
-const LIBRARY_SERVICES_INTENT='Library_Services'
-
-const app=dialogflow()
-app.intent(WELLCOME_INTENT, (conv)={
-	conv.ask("Wellcome to UVA Library Services! Ask for archives info") 		
-})
-app.intent(FALLBACK_INTENT, (conv)=> {
-	conv.ask("I didn't understand your request")	
-})
-app.intent(LIBRARY_SERVICES_INTENT, (conv) =>{
-     conv.ask("http://small.library.virginia.edu/collections/university-of-virginia-archives/")
-})
-exports.dialogflowFirebaseFulfillment=functions.https.onRequest(app)
-
-*/
