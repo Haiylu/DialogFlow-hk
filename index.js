99c30fc60a7bd1efd2cd11c4af897bf238d4572e
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-07 22:07:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const rp = require('request-promise-native ');
+/*const rp = require('request-promise-native ');
 function servicesTest(agent){
     var final=" ";
     var hk={ url:'https://api.devhub.virginia.edu/v1/library/services/', 
@@ -24,3 +24,24 @@ return rp(hk)
 module.exports = {
 		servicesTest:servicesTest
 }
+
+//Library_Services
+*/
+const functions=require('firebase-functions')
+const {dialogflow}=require('actions-on-google')
+
+const WELLCOME_INTENT='Default Welcome Intent'
+const FALLBACK_INTENT='Default Fallback Intent'
+const LIBRARY_SERVICES_INTENT='Library_Services'
+
+const app=dialogflow()
+app.intent(WELLCOME_INTENT, (conv)={
+	conv.ask("Wellcome to UVA Library Services! Ask for archives info") 		
+})
+app.intent(FALLBACK_INTENT, (conv)=> {
+	conv.ask("I didn't understand your request")	
+})
+app.intent(LIBRARY_SERVICES_INTENT, (conv) =>{
+     conv.ask("http://small.library.virginia.edu/collections/university-of-virginia-archives/")
+})
+exports.dialogflowFirebaseFulfillment=functions.https.onRequest(app)
