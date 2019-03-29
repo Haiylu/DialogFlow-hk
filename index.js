1400d481d3ad89b922f20dfdc3482e034738dd64
Haiylu <Haiylu.Kibret@gmail.com>
2019-03-29 14:11:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ return rp(hk)
 		//console.log(services[i].emailAdress);
 		if(title==services[i].title){
 			console.log(services[i].siteLink);
-			//console.log(services[i].phoneNumber);
+			console.log(services[i].phoneNumber);
 			//console.log(services[i].emailAddress);
 			//if(agent.Services_info=="phone number"){
 				//agent.add(services[i].phoneNumber)
