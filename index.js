13ca9d02ed679436ae0cfbedfde079e41ac94a33
Haiylu <Haiylu.Kibret@gmail.com>
2019-04-03 12:54:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,14 @@ return rp(hk)
 				//console.log(services[i].siteLink);
 			if(servicesName==services[i].title){
 			//console.log(services[i].siteLink);
-				agent.add(services[i].siteLink);
-				agent.add(services[i].phoneNumber);
-				agent.add(services[i].emailAddress);
+					agent.add(services[i].siteLink);
+			}
+				if(services_info==services[i].phoneNumber){
+					agent.add(services[i].phoneNumber);
+				}
+				if(services_info==services[i].emailAddress){
+					agent.add(services[i].emailAddress);
+				}	
 			}
 			}
 			//console.log(services[0].id);
